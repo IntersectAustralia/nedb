d433b448e89d76a7c1371d36f4a471b58a191674
Ilya Anisimoff <ilya@intersect.org.au>
2013-01-10 10:13:35 +1100

    UNEHERB-390: Fix AccessionValidator to enable record updating

--- lib/accession_validator.rb
+++ lib/accession_validator.rb
@@ -1,10 +1,12 @@
 class AccessionValidator < ActiveModel::Validator
 
   def validate(record)
-  	unless record.nil?
+    # using where instead of find so it doesn't raise ActiveRecord::RecordNotFound
+    existing_record = Specimen.where(:id => record.id).first
+  	unless record.nil? or record.equal? existing_record
   	  if record.id < 1
   	  	record.errors[:base] << 'Accession number needs to be greater than 0'
-  	  elsif not Specimen.where(:id => record.id).empty?
+  	  elsif not existing_record.nil?
   	  	record.errors[:base] << "Supplied accession number #{record.id} already in use"
   	  elsif Specimen.last.nil? or record.id >= Specimen.last.id
   	  	record.errors[:base] << "Supplied accession number #{record.id} is out of range"
