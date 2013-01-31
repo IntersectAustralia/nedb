09a285c61360ee974f5bfb96e530a0668f32c1d6
Ilya Anisimoff <ilya@intersect.org.au>
2013-02-01 10:24:20 +1100

    UNEHERB-390: Update accession_validator logic
    Updates accession_validator logic to handle both new records and updated records

--- lib/accession_validator.rb
+++ lib/accession_validator.rb
@@ -3,7 +3,7 @@ class AccessionValidator < ActiveModel::Validator
   def validate(record)
     # using where instead of find so it doesn't raise ActiveRecord::RecordNotFound
     existing_record = Specimen.where(:id => record.id).first
-  	unless record.nil? or record.eql? existing_record
+  	unless not record.new_record? and record.eql? existing_record
   	  if record.id < 1
   	  	record.errors[:base] << 'Accession number needs to be greater than 0'
   	  elsif not existing_record.nil?
