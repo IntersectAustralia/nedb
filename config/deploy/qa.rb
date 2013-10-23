bfbd3384dc543edc9be022c29ebbe47df35f99c6
Georgina Edwards <gc_edwards@yahoo.com.au>
2013-10-23 13:41:25 +1100

    Fix QA server name

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,4 +1,4 @@
-role :web, "gsw1-herbarium-qa-vm.intersect.org.au"                          # Your HTTP server, Apache/etc
-role :app, "gsw1-herbarium-qa-vm.intersect.org.au"                         # This may be the same as your `Web` server
-role :db,  "gsw1-herbarium-qa-vm.intersect.org.au", :primary => true       # This is where Rails migrations will run
+role :web, "ic2-herbarium-qa-vm.intersect.org.au"                          # Your HTTP server, Apache/etc
+role :app, "ic2-herbarium-qa-vm.intersect.org.au"                         # This may be the same as your `Web` server
+role :db,  "ic2-herbarium-qa-vm.intersect.org.au", :primary => true       # This is where Rails migrations will run
 
