dba9f2d780183db4d209406c7ed8975641f83657
Sean Lin <seanl@intersect.org.au>
2014-08-06 11:52:54 +1000

    Fix for DEVSUPPORT-1153, add warnings if from greater than to

--- app/helpers/coordinates_validator.rb
+++ app/helpers/coordinates_validator.rb
@@ -5,30 +5,35 @@ class CoordinatesValidator < ActiveModel::Validator
   end
 
   def validate_coordinates
-    lat_deg_from = @search_params[:latitude_degrees_greater_than_or_equal_to].to_i
-    lat_min_from = @search_params[:latitude_minutes_greater_than_or_equal_to].to_i
-    lat_sec_from = @search_params[:latitude_seconds_greater_than_or_equal_to].to_i
-    lat_deg_to = @search_params[:latitude_degrees_less_than_or_equal_to].to_i
-    lat_min_to = @search_params[:latitude_minutes_less_than_or_equal_to].to_i
-    lat_sec_to = @search_params[:latitude_seconds_less_than_or_equal_to].to_i
-
-    long_deg_from = @search_params[:longitude_degrees_greater_than_or_equal_to].to_i
-    long_min_from = @search_params[:longitude_minutes_greater_than_or_equal_to].to_i
-    long_sec_from = @search_params[:longitude_seconds_greater_than_or_equal_to].to_i
-    long_deg_to = @search_params[:longitude_degrees_less_than_or_equal_to].to_i
-    long_min_to = @search_params[:longitude_minutes_less_than_or_equal_to].to_i
-    long_sec_to = @search_params[:longitude_seconds_less_than_or_equal_to].to_i
-
+    lat_deg_from, lat_min_from, lat_sec_from, lat_deg_to, lat_min_to, lat_sec_to, long_deg_from, long_min_from, long_sec_from, long_deg_to, long_min_to, long_sec_to = sanitize_lat_and_long
     validate(lat_deg_from, lat_min_from, lat_sec_from, "Latitude from", :latitude)
     validate(lat_deg_to, lat_min_to, lat_sec_to, "Latitude to", :latitude)
     validate(long_deg_from, long_min_from, long_sec_from, "Longitude from", :longitude)
     validate(long_deg_to, long_min_to, long_sec_to, "Longitude to", :longitude)
+    validate_from_less_than_or_equal_to_to(lat_deg_from, lat_min_from, lat_sec_from, lat_deg_to, lat_min_to, lat_sec_to, :latitude)
+    validate_from_less_than_or_equal_to_to(long_deg_from, long_min_from, long_sec_from, long_deg_to, long_min_to, long_sec_to, :longitude)
     @record
   end
 
-  #TODO: fix up invalid for field == 0
+  def sanitize_lat_and_long
+    [
+      :latitude_degrees_greater_than_or_equal_to,
+      :latitude_minutes_greater_than_or_equal_to,
+      :latitude_seconds_greater_than_or_equal_to,
+      :latitude_degrees_less_than_or_equal_to,
+      :latitude_minutes_less_than_or_equal_to,
+      :latitude_seconds_less_than_or_equal_to,
+      :longitude_degrees_greater_than_or_equal_to,
+      :longitude_minutes_greater_than_or_equal_to,
+      :longitude_seconds_greater_than_or_equal_to,
+      :longitude_degrees_less_than_or_equal_to,
+      :longitude_minutes_less_than_or_equal_to,
+      :longitude_seconds_less_than_or_equal_to
+    ].collect {|x| @search_params[x].blank? ? nil : @search_params[x].to_i}
+  end
+
   def validate(deg, min, sec, field_name, type)
-    if not_empty?(deg)
+    if deg
       if type.eql? :latitude
         if deg > 90 or deg < 0
           @record = "You have entered an invalid degree for #{field_name}."
@@ -40,42 +45,32 @@ class CoordinatesValidator < ActiveModel::Validator
       end
     else
       # min and/or sec without deg is invalid
-      if not_empty?(min) || not_empty?(sec)
+      if min || sec
         @record = "Enter degrees for #{field_name}."
       end
     end
 
     # sec without min is invalid
-    if not_empty?(sec) && empty?(min)
+    if sec && min.nil?
       @record = "Enter minutes for #{field_name}."
     end
-  #  # sec/min not empty - check for year
-  #  if not_empty? deg
-  #    if type.eql? :latitude and (deg > 90 or deg < 0)
-  #      @record = "You have entered an invalid degree for #{field_name}."
-  #    elsif type.eql? :longitude and (deg > 180 or deg < 0)
-  #      @record = "You have entered an invalid degree for #{field_name}."
-  #    elsif min > 60 or min < 0
-  #      @record = "You have entered an invalid minute for #{field_name}."
-  #    elsif sec > 60 or sec < 0
-  #      @record = "You have entered an invalid second for #{field_name}."
-  #    end
-  #  #check that min is not empty if sec
-  #elsif empty? min and not_empty? sec
-  #    @record = "Enter minutes for #{field_name}."
-  #  end
-  #else
-  #  if not_empty? min or not_empty? sec and empty? deg
-  #    @record = "Enter degrees for #{field_name}."
-  #  end
   end
 
-  def empty?(str)
-    not not_empty?(str)
-  end
+  def validate_from_less_than_or_equal_to_to(deg_from, min_from, sec_from, deg_to, min_to, sec_to, type)
+    # Only validate if other validations pass
+    unless @record
+      from = deg_from * 3600 if deg_from
+      from = from + min_from * 60 if min_from
+      from = from + sec_from if sec_from
+      to = deg_to * 3600 if deg_to
+      to = to + min_to * 60 if min_to
+      to = to + sec_to if sec_to
 
-  def not_empty?(str)
-    str != nil && str.to_i > 0
+      if from && to && from > to
+        field_title = type.to_s.capitalize
+        @record = "#{field_title} to value must be greater than #{field_title} from value"
+      end
+    end
   end
 
 end
